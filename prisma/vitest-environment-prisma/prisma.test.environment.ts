122a520d03d2c3b12acf1f579c1ae0c2f4d2ce7a
Renan <renan.fachin@hotmail.com>
2023-05-05 17:25:01 -0300

    Cria testes end to end de registro e autenticação

--- prisma/vitest-environment-prisma/prisma.test.environment.ts
+++ prisma/vitest-environment-prisma/prisma.test.environment.ts
@@ -1,12 +1,48 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+// postgresql://docker:docker@localhost:5432/apisolidfindfriend?schema=public
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  // Trocando a url do db, trocando o public pela variável recebida na função
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Executou')
+    // Gerando um banco de dados único para cada switch de testes
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    // Sobreescrevendo as variáveis de ambiente
+    process.env.DATABASE_URL = databaseURL
+
+    // Rodando as migrations
+    // a flag deploy é para o prisma não fazer uma comparação com os bancos antigos
+    execSync('npx prisma migrate deploy')
 
     return {
-      teardown() {},
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
+      },
     }
   },
 }
--- src/http/controllers/organizations/authenticate.spec.ts
+++ src/http/controllers/organizations/authenticate.spec.ts
@@ -2,7 +2,7 @@ import { it, describe, expect, beforeAll, afterAll } from 'vitest'
 import request from 'supertest'
 import { app } from '@/app'
 
-describe('[e2e] - Register', () => {
+describe('[e2e] - Authenticate', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -11,8 +11,8 @@ describe('[e2e] - Register', () => {
     await app.close()
   })
 
-  it('Should be able to register an organization', async () => {
-    const response = await request(app.server).post('/register').send({
+  it('Should be able to authenticate', async () => {
+    await request(app.server).post('/register').send({
       name: 'ORG - TEST',
       responsable_name: 'Register Test',
       email: 'funcionando4@email.com',
@@ -22,6 +22,14 @@ describe('[e2e] - Register', () => {
       postal_code: '92000000',
     })
 
-    expect(response.statusCode).toEqual(201)
+    const response = await request(app.server).post('/session').send({
+      email: 'funcionando4@email.com',
+      password: '1234567',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      authToken: expect.any(String),
+    })
   })
 })
